f43216b0c8a491e3b0948df6a5d8b00632cfae17
oscarcederberg <os7138ce-s@student.lu.se>
2025-10-07 07:52:16 +0200

    Fix nvim lsp migration issues

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,5 @@
 return {
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v4.x',
-    lazy = true,
-    config = false,
-  },
+  -- LSP Installer
   {
     'williamboman/mason.nvim',
     lazy = false,
@@ -43,7 +38,6 @@ return {
   -- LSP
   {
     'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
@@ -51,65 +45,38 @@ return {
       { 'williamboman/mason-lspconfig.nvim' },
     },
     config = function()
-      local lsp_zero = require('lsp-zero')
+      -- Keymaps
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local opts = { buffer = bufnr }
 
-      -- lsp_attach is where you enable features that only work
-      -- if there is a language server active in the file
-      local lsp_attach = function(client, bufnr)
-        local opts = { buffer = bufnr }
-
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>',
-          { desc = "Hover information", unpack(opts) })
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>',
-          { desc = "Go to definition", unpack(opts) })
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>',
-          { desc = "Go to declaration", unpack(opts) })
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>',
-          { desc = "List implementation", unpack(opts) })
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>',
-          { desc = "Go to type definition", unpack(opts) })
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>',
-          { desc = "List references", unpack(opts) })
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>',
-          { desc = "Display signature help", unpack(opts) })
-        vim.keymap.set('n', 'gh', '<cmd>ClangdSwitchSourceHeader<cr>',
-          { desc = "Clangd switch between header and source", unpack(opts) })
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>',
-          { desc = "Rename symbol", unpack(opts) })
-        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
-          { desc = "Format code", unpack(opts) })
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>',
-          { desc = "Select code action", unpack(opts) })
-      end
-
-      lsp_zero.extend_lspconfig({
-        sign_text = true,
-        lsp_attach = lsp_attach,
-        capabilities = require('cmp_nvim_lsp').default_capabilities()
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', { desc = "Hover information", unpack(opts) })
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { desc = "Go to definition", unpack(opts) })
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>',
+            { desc = "Go to declaration", unpack(opts) })
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>',
+            { desc = "List implementation", unpack(opts) })
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>',
+            { desc = "Go to type definition", unpack(opts) })
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', { desc = "List references", unpack(opts) })
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>',
+            { desc = "Display signature help", unpack(opts) })
+          vim.keymap.set('n', 'gh', '<cmd>ClangdSwitchSourceHeader<cr>',
+            { desc = "Clangd: switch header/source", unpack(opts) })
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', { desc = "Rename symbol", unpack(opts) })
+          vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+            { desc = "Format code", unpack(opts) })
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>',
+            { desc = "Select code action", unpack(opts) })
+        end,
       })
 
-      lsp_zero.setup_servers({
-        'clangd',
-        'gopls',
-        'lua_ls',
-        'marksman',
-        'pylsp',
-        'rust_analyzer',
-      })
+      -- Mason
+      require('mason').setup()
 
-      lsp_zero.format_on_save({
-        format_opts = {
-          async = false,
-          timeout_ms = 10000,
-        },
-        servers = {
-          ['gopls'] = { 'go' },
-          ['lua_ls'] = { 'lua' },
-          ['rust_analyzer'] = { 'rust' },
-        }
-      })
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      require('mason').setup({})
       require('mason-lspconfig').setup({
         ensure_installed = {
           'clangd',
@@ -120,12 +87,39 @@ return {
           'rust_analyzer',
         },
         handlers = {
-          -- this first function is the "default handler"
-          -- it applies to every language server without a "custom handler"
+          -- Default handler for all installed servers
           function(server_name)
-            require('lspconfig')[server_name].setup({})
+            vim.lsp.config(server_name, {
+              capabilities = capabilities,
+              flags = { debounce_text_changes = 150 },
+              settings = {}, -- optional per-server settings
+            })
+            vim.lsp.enable({ server_name })
           end,
-        }
+        },
+      })
+
+      -- Format-on-save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function(event)
+          local bufnr = event.buf
+          local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+          local allowed = { go = true, lua = true, rust = true }
+
+          if not allowed[ft] then return end
+
+          local clients = vim.lsp.get_clients({ bufnr = bufnr })
+          for _, client in ipairs(clients) do
+            if client.supports_method("textDocument/formatting") then
+              vim.lsp.buf.format({
+                async = false,
+                timeout_ms = 10000,
+                bufnr = bufnr,
+              })
+              return
+            end
+          end
+        end,
       })
     end
   }
